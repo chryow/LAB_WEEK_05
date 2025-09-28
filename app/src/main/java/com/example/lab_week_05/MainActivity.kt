a50f2340d2c9227bb5f8cecb77a083365430fbb1
Wilson <modestuwilson@gmail.com>
2025-09-28 20:56:22 +0700

    Commit No. 3 - Load and Display Cat Image using Glide

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -15,6 +15,7 @@ import retrofit2.Callback
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.converter.moshi.MoshiConverterFactory
+import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +35,14 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.api_response)
     }
 
+    private val imageResultView: ImageView by lazy {
+        findViewById(R.id.image_result)
+    }
+
+    private val imageLoader: ImageLoader by lazy {
+        GlideLoader(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -50,10 +59,16 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<ImageData>>, response: Response<List<ImageData>>) {
                 if(response.isSuccessful){
                     val image = response.body()
-                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    val firstImage = image?.firstOrNull()?.imageUrl.orEmpty()
+                    if(firstImage.isNotBlank()){
+                        imageLoader.loadImage(firstImage, imageResultView)
+                    }
+                    else{
+                        Log.d(MAIN_ACTIVITY, "Missing image url")
+                    }
                     apiResponseView.text = getString(R.string.image_placeholder, firstImage)
                 }
-                else {
+                else{
                     Log.e(MAIN_ACTIVITY, "Failed to get response\n" + response.errorBody()?.string().orEmpty())
                 }
             }
